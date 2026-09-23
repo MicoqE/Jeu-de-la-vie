5d7b0eeb80330b1c63b2cd82f902772ac2f13447
MicoqE <michelcoq19@gmail.com>
2026-09-23 16:27:26 +0000

    Ajout de la fonction etape_suivante dans main.py et correction de la grammaire dans le README

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Implémentation en Python du Jeu de la Vie, le célèbre automate cellulaire mat
 
 # Les règles
 - si la cellule vis
-    - si les parmis les cellules autours 2 ou 3 sont en vie alors la cellules vie.
+    - si parmis les cellules autours 2 ou 3 sont en vie alors la cellules vie.
     - mais si le nombre de cellules en vie autours de la cellules est inférieur à deux ou supérieur à 3 alors elle meurt
 - mais si la cellule ne vie pas
     - si le nombre de cellule en vie autour est exactement 3 alors elle prend vie
@@ -13,4 +13,4 @@ Implémentation en Python du Jeu de la Vie, le célèbre automate cellulaire mat
 tableau de i par i
 
 # Calcule de l'état à la base des règles
-On vérifi l'était de chaque cellule autour de celle sur lequel le curseur sera et on sauvegard son état futur puis on fait de même avec la cellule suivante. à la fin on appliqueq l'état suivant de toute les cellules puis on recommence le processuce.
+On vérifi l'était de chaque cellule autour de celle sur lequel le curseur sera et on sauvegard son état futur puis on fait de même avec la cellule suivante. à la fin on applique l'état suivant de toute les cellules puis on recommence le processuce.
--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import numpy as np
 def init_grille(largeur, hauteur):
     return np.random.choice([True, False], size=(largeur, hauteur))
     
-grille = init_grille(3, 3)
+grille = init_grille(5, 5)
 
-"""pour di dans [-1, 0, 1] :
+# Algorythme de comptage des voisins
+"""
+pour di dans [-1, 0, 1] :
     pour dj dans [-1, 0, 1] :
         si di == 0 et dj == 0 : ignorer (c'est la cellule elle-même)
         si i+di et j+dj sont dans les limites de la grille :
-            compter ce voisin s'il est vivant"""
+            compter ce voisin s'il est vivant
+"""
 
 def compter_voisins(grille, i, j):
     nb = 0 # Nombre de voisin vivant
@@ -22,8 +25,22 @@ def compter_voisins(grille, i, j):
                     nb += 1
     return nb
 
+def etape_suivante(grille):
+    grille_futur = np.full(grille.shape, None)
+    for i in range(len(grille)):
+        for j in range(len(grille[i])):
+            voisin = compter_voisins(grille, i, j)
+            if grille[i][j] == True:
+                if voisin == 2 or voisin == 3:
+                    grille_futur[i][j] = True
+                else:
+                    grille_futur[i][j] = False
+            else:
+                if voisin == 3:
+                    grille_futur[i][j] = True
+                else:
+                    grille_futur[i][j] = False
+    return grille_futur
+            
 print(grille)
-for i in range(len(grille)):
-    for j in range(len(grille[i])):
-        voisin = compter_voisins(grille, i, j)
-        print(voisin)
+print(etape_suivante(grille))
